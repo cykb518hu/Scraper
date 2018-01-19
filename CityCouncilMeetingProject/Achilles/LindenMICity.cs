f77d861ff01fda6c07a3c08db5568b62909761c4
achilles.hu <achilles.hu@CHELPR-NW10002>
2018-01-19 18:14:31 +0800

    a new city

---
+++ CityCouncilMeetingProject/Achilles/LindenMICity.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace CityCouncilMeetingProject
+{
+    public class LindenMICity : City
+    {
+        private List<string> docUrls = null;
+
+        public LindenMICity()
+        {
+            cityEntity = new CityInfo()
+            {
+                CityId = "LindenMICity",
+                CityName = "Linden MI",
+                CityUrl = "www.lindenmi.us",
+                StateCode = "MI"
+            };
+
+            this.localQueryFile = string.Format("{0}_Queries.csv", this.cityEntity.CityName);
+            this.localDocFile = string.Format("{0}_Docs.csv", this.cityEntity.CityName);
+            localDirectory = string.Format("{0}\\{1}", Environment.CurrentDirectory, cityEntity.CityId);
+
+            if (!Directory.Exists(localDirectory))
+            {
+                Directory.CreateDirectory(localDirectory);
+            }
+
+            this.docUrls = File.ReadAllLines("LindenMICityMI_Urls.txt").ToList();
+        }
+
+        public void DownloadCouncilPdfFiles()
+        {
+           var docs = this.LoadDocumentsDoneSQL();
+            var queries = this.LoadQueriesDoneSQL();
+          //var docs = new List<Documents>();
+          //  var queries = new List<QueryResult>();
+            WebClient c = new WebClient();
+            HtmlWeb web = new HtmlWeb();
+            Dictionary<Regex, string> dateRegFormatDic = new Dictionary<Regex, string>();
+            dateRegFormatDic.Add(new Regex("[0-9]{1}-[0-9]{2}-[0-9]{2}"), "M-dd-yy");
+            dateRegFormatDic.Add(new Regex("[0-9]{1}-[0-9]{1}-[0-9]{2}"), "M-d-yy");
+            dateRegFormatDic.Add(new Regex("[a-zA-Z]+[\\s]{0,2}[0-9]{1,2},[\\s]{0,2}[0-9]{4}"), "");
+            foreach (string url in this.docUrls)
+            {
+                var subUrl = url.Split('*')[1];
+                var category = url.Split('*')[0];
+                HtmlDocument doc = web.Load(subUrl);
+                HtmlNodeCollection list = doc.DocumentNode.SelectNodes("//a[contains(@href,'/images/')]");
+                foreach (var r in list)
+                {
+                    var dateConvert = false;
+                    DateTime meetingDate = DateTime.MinValue;
+                    foreach (var dateRegKey in dateRegFormatDic.Keys)
+                    {
+                        string format = dateRegFormatDic[dateRegKey];
+                        string meetingDateText = dateRegKey.Match(r.InnerText).ToString();
+                        if (string.IsNullOrWhiteSpace(format))
+                        {
+                            if (DateTime.TryParse(meetingDateText, out meetingDate))
+                            {
+                                dateConvert = true;
+                                break;
+                            }
+                        }
+                        if (DateTime.TryParseExact(meetingDateText, format, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out meetingDate))
+                        {
+                            dateConvert = true;
+                            break;
+                        }
+
+                    }
+                    if (!dateConvert)
+                    {
+                        Console.WriteLine(r.InnerText);
+                        Console.WriteLine("date format incorrect...");
+                        continue;
+                    }
+
+                    if (meetingDate < this.dtStartFrom)
+                    {
+                        Console.WriteLine("Early...");
+                        continue;
+                    }
+                    var type = Path.GetExtension(r.Attributes["href"].Value);
+                    type = type.Replace(".", "");
+                   // Console.WriteLine(string.Format("type:{0},category:{1},datetime:{2}", type, category,meetingDate.ToString("yyyy-MM-dd")));
+                     this.ExtractADoc(c, this.cityEntity.CityUrl+ r.Attributes["href"].Value, category, type, meetingDate, ref docs, ref queries);
+                }
+            }
+            Console.WriteLine("docs:" + docs.Count + "--- query:" + queries.Count);
+        }
+    }
+}
