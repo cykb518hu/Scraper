1940b1e31326fe175de8c0aca13936f189fca9e6
cykb518hu <463372303@qq.com>
2018-03-05 09:00:17 +0800

    add new city

---
+++ CityCouncilMeetingProject/Achilles/MiltonAntrimTownshipMI.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace CityCouncilMeetingProject
+{
+    public class MiltonAntrimTownshipMI : City
+    {
+        private List<string> docUrls = null;
+
+        public MiltonAntrimTownshipMI()
+        {
+            cityEntity = new CityInfo()
+            {
+                CityId = "MiltonAntrimTownshipMI",
+                CityName = "Milton-Antrim Charter Township",
+                CityUrl = "http://www.miltontownship.org/",
+                StateCode = "MI"
+            };
+
+            this.localQueryFile = string.Format("{0}_Queries.csv", this.cityEntity.CityName);
+            this.localDocFile = string.Format("{0}_Docs.csv", this.cityEntity.CityName);
+            localDirectory = string.Format("{0}\\{1}", Environment.CurrentDirectory, cityEntity.CityId);
+
+            if (!Directory.Exists(localDirectory))
+            {
+                Directory.CreateDirectory(localDirectory);
+            }
+
+            this.docUrls = File.ReadAllLines("MiltonAntrimTownshipMI_Urls.txt").ToList();
+        }
+
+        public void DownloadCouncilPdfFiles()
+        {
+            var docs = this.LoadDocumentsDoneSQL();
+            var queries = this.LoadQueriesDoneSQL();
+            // var docs = new List<Documents>();
+            // var queries = new List<QueryResult>();
+            WebClient c = new WebClient();
+            HtmlWeb web = new HtmlWeb();
+            foreach (string url in this.docUrls)
+            {
+                var subUrl = url.Split('*')[1];
+                var category = url.Split('*')[0];
+                HtmlDocument doc = web.Load(subUrl);
+                HtmlNodeCollection list = doc.DocumentNode.SelectNodes("//a[contains(@href,'.pdf')]");
+                foreach (var r in list)
+                {
+                    if(string.IsNullOrWhiteSpace(r.InnerText))
+                    {
+                        continue;
+                    }
+                    var dateConvert = false;
+                    var newDateStr = "";
+                    if (category == "Park Commission")
+                    {
+                        newDateStr = r.InnerText.Replace(" ","").Substring(0, 4);
+                    }
+                    else
+                    {
+                        var dateStr = r.Attributes["href"].Value;
+                        dateStr = dateStr.Replace("spec-", "");
+                        var year = dateStr.Substring(0, 4);
+                        var subDate = dateStr.Substring(dateStr.IndexOf("/") + 1, dateStr.IndexOf(".") - dateStr.IndexOf("/") - 1).ToUpper();
+                        if (subDate.ToCharArray().Any(char.IsDigit))
+                        {
+                            newDateStr = year + "-" + subDate;    
+                        }
+                        else
+                        {
+                            newDateStr = year;
+                        }
+                       
+                      
+                    }
+                    DateTime meetingDate = DateTime.MinValue;
+                    string[] formats = { "yyyy-MMMdd", "yyyy-MMMd","yyyy" };
+                    if (DateTime.TryParseExact(newDateStr, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out meetingDate))
+                    {
+                        dateConvert = true;
+                    }
+                    if (!dateConvert)
+                    {
+                        Console.WriteLine(r.InnerText + ":" + newDateStr);
+                        Console.WriteLine("date format incorrect...");
+                        continue;
+                    }
+
+                    if (meetingDate < this.dtStartFrom)
+                    {
+                        Console.WriteLine("Early...");
+                        continue;
+                    }
+                    //Console.WriteLine(string.Format("datestr:{0},meeting:{1}", r.Attributes["href"].Value, meetingDate.ToString("yyyy-MM-dd")));
+                     this.ExtractADoc(c, this.cityEntity.CityUrl + r.Attributes["href"].Value, category, "pdf", meetingDate, ref docs, ref queries);
+                }
+            }
+            Console.WriteLine("docs:" + docs.Count + "--- query:" + queries.Count);
+            // Console.ReadKey();
+        }
+    }
+}
---
+++ CityCouncilMeetingProject/Achilles/MiltonCassTownshipMI.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace CityCouncilMeetingProject
+{
+    public class MiltonCassTownshipMI : City
+    {
+        private List<string> docUrls = null;
+
+        public MiltonCassTownshipMI()
+        {
+            cityEntity = new CityInfo()
+            {
+                CityId = "MiltonCassTownshipMI",
+                CityName = "Milton-Cass Charter Township",
+                CityUrl = "http://www.miltontwp.org",
+                StateCode = "MI"
+            };
+
+            this.localQueryFile = string.Format("{0}_Queries.csv", this.cityEntity.CityName);
+            this.localDocFile = string.Format("{0}_Docs.csv", this.cityEntity.CityName);
+            localDirectory = string.Format("{0}\\{1}", Environment.CurrentDirectory, cityEntity.CityId);
+
+            if (!Directory.Exists(localDirectory))
+            {
+                Directory.CreateDirectory(localDirectory);
+            }
+
+            this.docUrls = File.ReadAllLines("MiltonCassTownshipMI_Urls.txt").ToList();
+        }
+
+        public void DownloadCouncilPdfFiles()
+        {
+            var docs = this.LoadDocumentsDoneSQL();
+            var queries = this.LoadQueriesDoneSQL();
+            // var docs = new List<Documents>();
+            // var queries = new List<QueryResult>();
+            WebClient c = new WebClient();
+            HtmlWeb web = new HtmlWeb();
+            Dictionary<Regex, string> dateRegFormatDic = new Dictionary<Regex, string>();
+            dateRegFormatDic.Add(new Regex("[A-Za-z]+[\\s]{0,1}[0-9]{1,2},[\\s]{0,1}[0-9]{4}"), " ");
+            dateRegFormatDic.Add(new Regex("[A-Za-z]+[\\s]{0,1}[0-9]{1,2}"), "Month");
+            dateRegFormatDic.Add(new Regex("[0-9]{2}-[0-9]{2}-[0-9]{2}"), "MM-dd-yy");
+            dateRegFormatDic.Add(new Regex("[0-9]{2}-[0-9]{1}-[0-9]{2}"), "MM-d-yy");
+            dateRegFormatDic.Add(new Regex("[0-9]{1}-[0-9]{2}-[0-9]{2}"), "M-dd-yy");
+            dateRegFormatDic.Add(new Regex("[0-9]{1}-[0-9]{1}-[0-9]{2}"), "M-d-yy");
+            foreach (string url in this.docUrls)
+            {
+                var subUrl = url.Split('*')[1];
+                var category = url.Split('*')[0];
+                HtmlDocument doc = web.Load(subUrl);
+                HtmlNodeCollection list = doc.DocumentNode.SelectNodes("//a[contains(@href,'/LinkClick.aspx')]");
+                foreach (var r in list)
+                {
+                    var dateConvert = false;
+                    DateTime meetingDate = DateTime.MinValue;
+
+                    foreach (var dateRegKey in dateRegFormatDic.Keys)
+                    {
+                        string format = dateRegFormatDic[dateRegKey];
+                        string meetingDateText = dateRegKey.Match(r.InnerText).ToString();
+                        if (string.IsNullOrWhiteSpace(format))
+                        {
+                            if (DateTime.TryParse(meetingDateText, out meetingDate))
+                            {
+                                dateConvert = true;
+                                break;
+                            }
+                        }
+                        if (format=="Month"&& !string.IsNullOrWhiteSpace(meetingDateText))
+                        {
+                            try
+                            {
+                                var year = r.ParentNode.ParentNode.ParentNode.ParentNode.ParentNode.PreviousSibling.PreviousSibling.InnerText;
+                                var dateStr = meetingDateText + ", " + year;
+                                if (DateTime.TryParse(dateStr, out meetingDate))
+                                {
+                                    dateConvert = true;
+                                    break;
+                                }
+                            }
+                            catch
+                            {
+                                continue;
+                            }
+
+                        }
+                        if (DateTime.TryParseExact(meetingDateText, format, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out meetingDate))
+                        {
+                            dateConvert = true;
+                            break;
+                        }
+
+                    }
+                    if (!dateConvert)
+                    {
+                        Console.WriteLine(r.InnerText);
+                        Console.WriteLine("date format incorrect...");
+                        continue;
+                    }
+
+                    if (meetingDate < this.dtStartFrom)
+                    {
+                        Console.WriteLine("Early...");
+                        continue;
+                    }
+                   // Console.WriteLine(string.Format("datestr:{0},meeting:{1}", r.InnerText, meetingDate.ToString("yyyy-MM-dd")));
+                     this.ExtractADoc(c, this.cityEntity.CityUrl + r.Attributes["href"].Value, category, "pdf", meetingDate, ref docs, ref queries);
+                }
+            }
+            Console.WriteLine("docs:" + docs.Count + "--- query:" + queries.Count);
+            // Console.ReadKey();
+        }
+    }
+}
